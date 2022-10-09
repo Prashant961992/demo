9ed3af0c890f6e7a9cc5dec771ae34a41beee527
Prashant Prajapati <pp961992@gmail.com>
2022-10-09 11:57:58 +0530

    remove unused import

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
-
 import com.example.demo.models.Book;
 import com.example.demo.service.BookService;
 
--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,8 +1,5 @@
 package com.example.demo.service;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
